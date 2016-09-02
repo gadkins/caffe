a728df81e81e3db916cbbd069a7b6177234f84a8
Grayson Adkins <grayson.w.adkins@gmail.com>
2016-09-01 21:16:08 -0700

    added array of parts return variable

--- scripts/matlab/part_mat2map.m
+++ scripts/matlab/part_mat2map.m
@@ -1,9 +1,10 @@
-function [cls_mask, inst_mask, part_mask] = part_mat2map(obj, img, pimap, desired_pid)
+function [cls_mask, inst_mask, part_mask, parts] = part_mat2map(obj, img, pimap, desired_pid)
 % obj is expected to be a single object from anno.objects()
 
 cls_mask = zeros(size(img,1), size(img,2), 'uint8');
 inst_mask = zeros(size(img,1), size(img,2), 'uint8');
 part_mask = zeros(size(img,1), size(img,2), 'uint8');
+parts = [];
 
 class_ind = obj.class_ind;
 silh = obj.mask;            % the silhouette mask of the object
@@ -13,6 +14,7 @@ cls_mask(silh) = class_ind;
     
 for pp = 1:numel(obj.parts)
     part_name = obj.parts(pp).part_name;
+    parts = strvcat(parts, part_name);
     assert(isKey(pimap{class_ind}, part_name));     % must define part index for every part
     assert(all(silh(obj.parts(pp).mask)));          % all part region is a subregion of the object
     pid = pimap{class_ind}(part_name);
@@ -24,5 +26,7 @@ for pp = 1:numel(obj.parts)
         continue
     end
 end
-
+if ~isempty(parts)
+    parts = cellstr(parts);
+end
 end
